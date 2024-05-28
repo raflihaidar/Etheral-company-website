5ce2b2bd9d98a561bba0bdb8475dc832ec4f9b5f
Rafli Haidar <rafli.haidar30@gmail.com>
2024-05-28 15:55:45 +0700

    fix bug at testimonial section

--- client/user/src/views/Home.js
+++ client/user/src/views/Home.js
@@ -170,11 +170,10 @@ export default class extends AbstractView {
         </button>
       </section>
       
-      <section class="testimonial" id="testimoni">
+      <section class="testimonial testimonial-slider swiper" id="testimoni">
         <h1>Testimoni.</h1>
-        <div clas="testimonials-slider swiper">
-        <div class="testimonial-list swiper-wrapper">
-          <div class="testimonial-figure swiper-slide">
+        <div class="testimonial-list">
+          <div class="testimonial-figure">
             <div class="testimonial-fill">
               <p>
                 <i class="bx bxs-quote-alt-left quote-icon-left"></i>
@@ -231,95 +230,8 @@ export default class extends AbstractView {
                 </div>
               </div>
           </div>
-          <div class="testimonial-figure swiper-slide">
-            <div class="testimonial-fill">
-              <p>
-                <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent rutrum,
-                tortor quis pretium laoreet, neque sem vestibulum lorem, quis convallis
-                neque magna condimentum augue. Cras interdum blandit eros id eleifend.
-                Sed lacinia augue ut lobortis aliquet.
-                <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-              </p>
-              </div>
-              <div class="testimonial-header">
-                <img src="/user/src/assets/user.jpg" alt="user" />
-                <div class="testimonial-profile">
-                  <p>Maulana Haqi</p>
-                  <p>maulanahaqi@mail.com</p>
-                </div>
-              </div>
-          </div>
-          <div class="testimonial-figure swiper-slide">
-            <div class="testimonial-fill">
-              <p>
-                <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent rutrum,
-                tortor quis pretium laoreet, neque sem vestibulum lorem, quis convallis
-                neque magna condimentum augue. Cras interdum blandit eros id eleifend.
-                Sed lacinia augue ut lobortis aliquet.
-                <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-              </p>
-              </div>
-              <div class="testimonial-header">
-                <img src="/user/src/assets/user.jpg" alt="user" />
-                <div class="testimonial-profile">
-                  <p>Maulana Haqi</p>
-                  <p>maulanahaqi@mail.com</p>
-                </div>
-              </div>
-          </div>
-          <div class="testimonial-figure swiper-slide">
-            <div class="testimonial-fill">
-              <p>
-                <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent rutrum,
-                tortor quis pretium laoreet, neque sem vestibulum lorem, quis convallis
-                neque magna condimentum augue. Cras interdum blandit eros id eleifend.
-                Sed lacinia augue ut lobortis aliquet.
-                <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-              </p>
-              </div>
-              <div class="testimonial-header">
-                <img src="/user/src/assets/user.jpg" alt="user" />
-                <div class="testimonial-profile">
-                  <p>Maulana Haqi</p>
-                  <p>maulanahaqi@mail.com</p>
-                </div>
-              </div>
-          </div>
-        </div>
-          <div class="swiper-pagination"></div>
         </div>
       </section>
     `;
   }
 }
-
-window.onload = () => {
-  new Swiper(".testimonials-slider", {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    slidesPerView: "auto",
-    pagination: {
-      el: ".swiper-pagination",
-      type: "bullets",
-      clickable: true,
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 20,
-      },
-
-      1200: {
-        slidesPerView: 3,
-        spaceBetween: 20,
-      },
-    },
-  });
-};
